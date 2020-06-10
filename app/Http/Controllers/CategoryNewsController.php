19a4c2f493443fbd9f5859e915a542565f04490d
mizan2832 <programmer620@gmail.com>
2020-06-10 17:03:05 +0600

    list view

--- app/Http/Controllers/CategoryNewsController.php
+++ app/Http/Controllers/CategoryNewsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use DB;
-
+use App\admin\Category;
 
 class CategoryNewsController extends Controller
 {
@@ -14,10 +14,16 @@ class CategoryNewsController extends Controller
         $name = explode("/",$request->url());
         $count = count($name);
         $category = $name[$count-1];
+        $category_name = $name[$count-1];
+       
         $category =DB::table('categories')->where('slug',$category)->first();
-      
         $news = DB::table('news')->where('category_id',$category->id)->get();
-        dd($news);
+
+        $category = Category::all();
+        return view('front.pages.single_category',['category_name' => $category_name])
+                                                 ->withNews($news)
+                                                 ->withCategories($category)
+                                                 ->withCategory_name($category_name);
 
     }
 }
